4d8ed534c4cc9f6c698c073590b2dee3431fe769
magn3tism <chapagainsakshyam@gmail.com>
2023-04-15 08:03:02 +0545

    Get anecdotes from backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { setAnecdotes } from "./reducers/anecdoteReducer";
+import anecdoteService from "./services/anecdoteService";
+
 import AnecdoteForm from "./components/AnecdotesForm";
 import AnecdotesList from "./components/AnecdotesList";
 import Filter from "./components/Filter";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+  }, []);
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -2,15 +2,6 @@ import uniqid from "uniqid";
 import _ from "lodash";
 import { createSlice } from "@reduxjs/toolkit";
 
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
 const asObject = (anecdote) => {
   return {
     content: anecdote,
@@ -19,11 +10,9 @@ const asObject = (anecdote) => {
   };
 };
 
-const initialState = anecdotesAtStart.map(asObject);
-
 const anecdoteSlice = createSlice({
   name: "anecdotes",
-  initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
       const anecdote = action.payload;
@@ -40,37 +29,17 @@ const anecdoteSlice = createSlice({
 
       return _.orderBy(newState, "votes", "desc");
     },
+
+    appendAnecdote(state, action) {
+      return state.concat(action.payload);
+    },
+
+    setAnecdotes(state, action) {
+      return action.payload;
+    },
   },
 });
 
-export const { createAnecdote, voteFor } = anecdoteSlice.actions;
+export const { createAnecdote, voteFor, appendAnecdote, setAnecdotes } =
+  anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
-
-// export const createAnecdote = (content) => {
-//   return { type: "ADD", payload: asObject(content) };
-// };
-
-// const anecdoteReducer = (state = initialState, action) => {
-//   console.log("----------Anecdote----------");
-//   console.log("State now: ", state);
-//   console.log("Action", action);
-
-//   switch (action.type) {
-//     case "VOTE":
-//       const newState = state.map((anecdote) => {
-//         if (anecdote.id === action.payload)
-//           return { ...anecdote, votes: ++anecdote.votes };
-//         return anecdote;
-//       });
-
-//       return _.orderBy(newState, "votes", "desc");
-
-//     case "ADD":
-//       return state.concat(action.payload);
-
-//     default:
-//       return state;
-//   }
-// };
-
-// export default anecdoteReducer;
